a7e81e8485552f5a5710f0f4b3d1b0c9a531cb76
owenvickery <owenvickery@gmail.com>
2020-02-14 13:40:46 +0000

    updated variables

--- metadynamics.py
+++ metadynamics.py
@@ -7,39 +7,41 @@ import multiprocessing as mp
 parser = argparse.ArgumentParser()
 parser.add_argument('-f', help='function',metavar='plot',type=str, choices= ['sort', 'skip', 'plot', 'boot', 'concat','strip', 'converge', 'frames', '1d_time', 'cv_plot', 'timecourse'], required=True)
 parser.add_argument('-input', help='name of input variables',metavar='protein',type=str, required=True)
-parser.add_argument('-save', help='save name',metavar='FES',type=str)
-parser.add_argument('-d1', help='plots 1d landscape', action='store_true')
-parser.add_argument('-bulk', help='plots bulk outline', action='store_true')
+parser.add_argument('-show', help='show interactive plots', action='store_true')
 parser.add_argument('-error', help='plots error instead of energy', action='store_true')
-parser.add_argument('-test', help='runs stripped down version of script', action='store_true')
-parser.add_argument('-s', help='start',metavar='1',type=int)
-parser.add_argument('-e', help='end',metavar='10',type=int)
+parser.add_argument('-info', help='print script information', action='store_true')
 args = parser.parse_args()
 options = vars(args)
 
-param={'circle_plot':False,'ellipse_plot':False, 'picture':False, '1d':False, 'bulk_outline':False, 'prefix':'fes_', '1d_location':'1d_landscapes', 'invert':1}
-circle = ['circle_area','ring_location']
-ellipse = ['ellipse_width', 'ellipse_height','ellipse_angle','ring_location']
-one_dimension = ['x_points', 'y_points', 'search_width']
+param={'circle_plot':False,'ellipse_plot':False, 'picture':False, '1d':False, 'bulk_outline':False, 'prefix':'fes_', '1d_location':'1d_landscapes'}
+circle = ['circle_area','circle_centers']
+ellipse = ['ellipse_width', 'ellipse_height','ellipse_angle','ellipse_centers']
+one_dimension = ['start_points', 'end_points', 'search_width']
 picture = ['picture_file','picture_loc']
 ### working
+
+if args.info:
+    functions.info()
+
 if args.f == 'sort':
-    setting = ['HILLS_sorted', 'HILLS'] 
+    setting = ['HILLS_sort', 'HILLS'] 
     param = functions.parameters(args.input, setting, param)
     functions.check_variable(setting, param)
-    os.system("awk \'{print $NF,$0}\' "+param['HILLS']+" | sort -n | cut -f2- -d\' \' > "+param['HILLS_sorted'])
-
+    if not os.path.exists(param['HILLS_sort']):
+        os.system("awk \'{print $NF,$0}\' "+param['HILLS']+" | sort -n | cut -f2- -d\' \' > "+param['HILLS_sort'])
+    else:
+        sys.exit('The file : '+param['HILLS_sort']+' already exists!')
 if args.f == 'skip':
     setting = ['HILLS_sort', 'HILLS', 'HILLS_skip'] 
     param = functions.parameters(args.input, setting, param)
     functions.check_variable(setting, param)
-    if not os.path.exists(param['HILLS_sorted']):
-        os.system("awk \'{print $NF,$0}\' "+param['HILLS']+" | sort -n | cut -f2- -d\' \' > "+param['HILLS_sorted'])
-    functions.skip_hills(param['HILLS_sorted'], param['HILLS_skip'])
+    if not os.path.exists(param['HILLS_sort']):
+        os.system("awk \'{print $NF,$0}\' "+param['HILLS']+" | sort -n | cut -f2- -d\' \' > "+param['HILLS_sort'])
+    functions.skip_hills(param['HILLS_sort'], param['HILLS_skip'])
 
 if args.f == 'plot':
-    setting = ['picture', 'bulk_values', 'fes', 'energy_max', 'invert','colour_bar_tick', 'CV1','CV2', 'interval_x', 'interval_y', 'bulk_outline', 
-               'circle_plot','ellipse_plot', '1d', 'step'] 
+    setting = ['picture', 'bulk_values', 'fes', 'plot_energy_max','plot_colour_bar_tick', 'CV1','CV2', 'plot_interval_x', 'plot_interval_y', 'bulk_outline', 
+               'circle_plot','ellipse_plot', '1d', 'plot_step'] 
     param = functions.parameters(args.input, setting, param)
     functions.check_variable(setting, param)
     if param['circle_plot']:
@@ -54,12 +56,12 @@ if args.f == 'plot':
     if param['picture']:
         param = functions.parameters(args.input, picture, param)
         functions.check_variable(picture, param)
-    functions.final_frame(param, args.error, args.save)
+    functions.final_frame(param, args.error, args.show)
 
 #### average z across frames
 if args.f == 'concat':
     setting = ['bulk_values', 'prefix', 'start', 'end'] 
-    param = functions.parameters(args.input, setting, {})    
+    param = functions.parameters(args.input, setting, param)    
     if os.path.exists(param['bulk_values']):
         coord=np.genfromtxt(param['bulk_values'], autostrip=True)
         sorted_X_s, sorted_Y_s, sorted_X_l, sorted_Y_l=coord[:,0],coord[:,1],coord[:,2],coord[:,3]
@@ -87,6 +89,14 @@ if args.f == 'concat':
 
 ### get values from timepoints
 if args.f == 'strip':
+    setting = ['circle_plot','ellipse_plot','bulk_values', 'prefix', 'start', 'end'] 
+    param = functions.parameters(args.input, setting, param)  
+    if param['circle_plot']:
+        param = functions.parameters(args.input, circle, param)
+        functions.check_variable(circle, param)
+    if param['ellipse_plot']:
+        param = functions.parameters(args.input, ellipse, param)
+        functions.check_variable(ellipse, param)
     pool = mp.Pool(mp.cpu_count())
     timecourse = pool.starmap_async(functions.strip, [(param, frame)  for frame in range(param['start'],param['end']+1)]).get()
     pool.close
@@ -99,26 +109,29 @@ if args.f == 'strip':
             enloc.write(line_outputs+'\n')
 
 if args.f == 'converge':
-    setting = ['bulk_values', 'HILLS_skipped', 'ring_location', 'start', 'end'] 
+    setting = ['bulk_values', 'HILLS_skip', 'start', 'end', 'converge_labels', 'converge_title_height',
+                'labels_size', 'circle_plot', 'ellipse_plot', 'converge_x_interval'] 
     param = functions.parameters(args.input, setting, param) 
     if param['circle_plot']:
         param = functions.parameters(args.input, circle, param)
     if param['ellipse_plot']:
         param = functions.parameters(args.input, ellipse, param)
-    functions.hills_converge(param)
+    functions.hills_converge(param, args.show)
 
 if args.f == 'frames':  ## needs updating
     for i in range(param['start'],param['end']+1):
         functions.get_frames(i)
 
 if args.f == '1d_time':
+    setting = ['start', 'end']+one_dimension 
+    param = functions.parameters(args.input, setting, param)
     if not os.path.exists(param['1d_location']):
         os.mkdir(param['1d_location'])
     frames=[]
     for fes_frame in range(param['start'],param['end']+1):
         if os.path.exists(param['prefix']+str(fes_frame)+'.dat'):
             cont=False
-            for site in range(1,len(param['ring_location'])+1):
+            for site in range(1,len(param['start_points'])+1):
                 if not os.path.exists(param['1d_location']+'/1D_landscape_site-'+str(site)+'_'+str(fes_frame)):
                     cont=True
                     break
@@ -129,10 +142,23 @@ if args.f == '1d_time':
             functions.average_1d(param, frame)
 
 if args.f == 'boot':
-    functions.bootstrap_1d(param)
+    setting = ['start', 'end', 'equilibration', 'stride', 'ref_min','ref_max', 'min_cv', 'cv_max', 
+                'boot_title_height', 'boot_label_y', 'boot_energy_max', 'boot_step_yaxis', 
+                'boot_labels_size']+one_dimension 
+    param = functions.parameters(args.input, setting, param)
+    functions.bootstrap_1d(param, args.show)
 
 if args.f == 'cv_plot':
-    functions.plot_CV(param)
+    setting = ['walker_range','cv_title_height','cv_tick_size', 'cv_labels_size', 'circle_plot', 
+                'ellipse_plot', 'CV1', 'CV2', 'cv_x_interval', 'cv_y_interval']
+    param = functions.parameters(args.input, setting, param)
+    if param['circle_plot']:
+        param = functions.parameters(args.input, circle, param)
+    if param['ellipse_plot']:
+        param = functions.parameters(args.input, ellipse, param)
+    functions.plot_CV(param, args.show)
 
 if args.f == 'timecourse':
-    functions.timecourse(param)
+    setting = ['cv_min', 'cv_max', 'step_xaxis', 'step_yaxis']+one_dimension
+    param = functions.parameters(args.input, setting, param)
+    functions.timecourse(param, args.show)
