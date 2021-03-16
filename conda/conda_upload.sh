f03609de7671cd617d3b66828d31d83e1dade7c9
owenvickery <owenvickery@gmail.com>
2021-03-16 09:40:24 +0000

    updated meta.yml

--- conda/conda_upload.sh
+++ conda/conda_upload.sh
@@ -1,5 +1,5 @@
 
-PKG_NAME=metadyanmics_analysis
+PKG_NAME=metadynamics_analysis
 USER=stansfeld_rg
 
 OS=linux-64
@@ -9,4 +9,5 @@ export CONDA_BLD_PATH=~/conda-bld
 export VERSION=`date +%Y.%m.%d`
 export package=$PKG_NAME-`date +%Y.%m.%d`-0
 conda build . 
+ls /home/travis/conda-bld/linux-64/
 anaconda -t $CONDA_UPLOAD_TOKEN upload -u $USER -l main $CONDA_BLD_PATH/$OS/$PKG_NAME-`date +%Y.%m.%d`-0.tar.bz2 --force
